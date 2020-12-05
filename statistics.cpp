ad242ca7c8796ce02716e41a8903cf3bf9c0ea23
hojkas <ivka.strnadova@gmail.com>
2020-12-05 18:52:38 +0100

    sum statistics work

--- statistics.cpp
+++ statistics.cpp
@@ -7,13 +7,43 @@
 */
 
 #include "discrete_simulator.hpp"
+#define output_separator "========================================"
 
 void Log::SimulatorState()
 {
+    std::cout << output_separator << std::endl
+              << "            SIMULATOR STATE" << std::endl
+              << output_separator << std::endl
+              << "TIME:" << std::endl
+              << "simulation time:   " << Simulator::last_effective_time << std::endl
+              << "start time     :   " << Simulator::start_time << std::endl
+              << "end time       :   " << Simulator::end_time << std::endl
+              << output_separator << std::endl
+              << "FACILITIES:   " << Simulator::facilities.size() << std::endl;
+    
+    for(const auto& f : Simulator::facilities) {
+        std::cout << f.second->name << std::endl << "     queued events:" << f.second->queue->queued_events.size() <<
+            std::endl << "     queue limit: ";
+        if(!f.second->queue->limited) std::cout << "none" << std::endl;
+        else std::cout << f.second->queue->limit << std::endl;
+    }
+
+    std::cout << output_separator << std::endl
+              << "STORAGES:   " << Simulator::storages.size() << std::endl;
+
+    for(const auto& f : Simulator::storages) {
+        std::cout << f.second->name << std::endl << "     capacity: " << f.second->capacity << std::endl <<
+            "     queued events: " << f.second->queue->queued_events.size() <<
+            std::endl << "     queue limit: ";
+        if(!f.second->queue->limited) std::cout << "none" << std::endl;
+        else std::cout << f.second->queue->limit << std::endl;
+    }
+
+    std::cout << output_separator << std::endl;
 
 }
 
 void Log::EventState(Event *event, std::string log_msg)
 {
-
+    std::cout << "[" << Simulator::last_effective_time << "] " << event->get_name() << ": " << log_msg << std::endl; 
 }
